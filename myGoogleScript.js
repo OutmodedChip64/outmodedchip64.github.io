b8f2d685fdf578719284e73ef508c582480cd407
OutmodedChip64 <cherryman656@gmail.com>
2019-07-25 15:07:40 -0700

    Initial commit

---
+++ myGoogleScript.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-131662500-2');
+
+
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: "ca-pub-8912092046189072",
+  enable_page_level_ads: true
+});
