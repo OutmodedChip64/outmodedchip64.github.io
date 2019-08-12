0fc30faf1a846c3b08133d0244f9a55234259ca4
OutmodedChip64 <cherryman656@gmail.com>
2019-08-12 15:54:33 -0700

    Fixed react page

---
+++ react/precache-manifest.a1b6aba1fb23f39302f4dd203e849fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf61231a1ea29770496eab487c72da73",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "91bbcbf350cf214d17ca",
+    "url": "/react/static/css/main.c03bbe60.chunk.css"
+  },
+  {
+    "revision": "88b2e3c39eb48715e717",
+    "url": "/react/static/js/2.26b8930a.chunk.js"
+  },
+  {
+    "revision": "91bbcbf350cf214d17ca",
+    "url": "/react/static/js/main.a02cc46f.chunk.js"
+  },
+  {
+    "revision": "80c8dd6f36788619c124",
+    "url": "/react/static/js/runtime~main.eb282349.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d05650d3d4687cafc24046baa05ae612.js"
+  "/react/precache-manifest.a1b6aba1fb23f39302f4dd203e849fea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
