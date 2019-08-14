bc65ed0fe68119cd50118bc573e9a0abe21a738a
OutmodedChip64 <cherryman656@gmail.com>
2019-08-14 14:04:21 -0700

    Updated icons and react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Tooltip from '@material-ui/core/Tooltip';
+
+
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
@@ -32,11 +39,85 @@ class Toggle extends React.Component {
   }
 }
 
+class SpinningLogo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {speed: "5"};
+  }
+
+  render() {
+    return (
+      <img
+        src={logo}
+        className="App-logo logoAnimate"
+        alt="logo"
+        style={{animation: "App-logo-spin infinite " + this.props.speed + "ms linear, Logo-Animate 2s cubic-bezier(0.4, 0, 0.2, 1)"}}
+      />
+    );
+  }
+}
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+});
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  const popperRef = React.useRef(null);
+  React.useEffect(() => {
+    if (popperRef.current) {
+      popperRef.current.update();
+    }
+  });
+
+  return (
+    <Tooltip
+      PopperProps={{
+        popperRef,
+      }}
+      open={open}
+      enterTouchDelay={0}
+      placement="top"
+      title={value}
+    >
+      {children}
+    </Tooltip>
+  );
+}
+
+function ContinuousSlider() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <br/>
+      <Typography id="continuous-slider" gutterBottom>
+        Logo Spin Speed
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Slider ValueLabelComponent={ValueLabelComponent} value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+const logoSlider = <ContinuousSlider />;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo logoAnimate" alt="logo" />
+        <SpinningLogo speed={logoSlider.value / 6} />
         <p className="editSrcAnimate">
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -50,6 +131,7 @@ function App() {
         </a>
         <br/>
         <Toggle />
+        {logoSlider}
       </header>
     </div>
   );
