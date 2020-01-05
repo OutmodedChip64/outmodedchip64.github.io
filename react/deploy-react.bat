6850e71d284acb35c3dfe22574fcba062898846b
OutmodedChip64 <cherryman656@gmail.com>
2020-01-05 15:32:59 -0800

    Added batch file to automate building react

---
+++ react/deploy-react.bat
@@ -0,0 +1,29 @@
+@echo off
+cd my-app
+echo * Changed directory to "\react\my-app"
+
+npm run build
+echo * Built react app
+
+cd ..\..
+echo * Changed directory to "\"
+
+rd /s /q ..\cubicle
+rd /s /q ..\playerpixel
+rd /s /q ..\static
+del ..\asset-manifest.json
+del ..\favicon.ico
+del ..\index.html
+del ..\logo192.png
+del ..\logo512.png
+del ..\manifest.json
+del ..\robots.txt
+del ..\*.js
+echo * Deleted previous react files
+
+xcopy /e "C:\Users\firen\OneDrive\Documents\GitHub\outmodedchip64.github.io\react\my-app\build" "C:\Users\firen\OneDrive\Documents\GitHub\outmodedchip64.github.io"
+echo * Copied new react files to root directory
+
+echo * J O B S   F I N I S H E D *
+
+pause
